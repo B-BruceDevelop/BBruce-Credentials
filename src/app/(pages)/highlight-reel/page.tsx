ce9fbcca0aae9b0c055a16f59fb4c420d3ab353d
B-BRUCE <b-bruce@MacBook-Air-de-Enric--B-BRUCE.local>
2024-12-10 14:13:52 +0100

    [fix]

--- src/app/(pages)/highlight-reel/page.tsx
+++ src/app/(pages)/highlight-reel/page.tsx
@@ -13,12 +13,18 @@ const HighlightReel = () => {
         prevPage="/latest-cases"
         nextPage="/trusted-brands"
       />
-      <main className="flex flex-grow  items-center justify-center w-full h-full p-4 gap-4 bg-red-500">
-        <LottieComponent
-          asset={spark}
-          loop={true}
-          className="w-full h-full"
-        />
+      <main className="flex flex-grow items-center justify-center w-full h-full max-h-[85vw] p-[1vw] ">
+       <video
+          className="w-full h-full object-cover rounded-xl "
+          autoPlay
+          
+          playsInline
+        >
+          <source
+            src="https://video.wixstatic.com/video/4cd4b0_80efd8431c8c46dda3dc9f05b6d6325a/1080p/mp4/file.mp4"
+            type="video/mp4"
+          />
+        </video>
       </main>
       <Footer
         title="We don’t aim to impress; it’s to create lasting impact, aligning each element with the brand’s spirit to resonate deeply with audiences."
--- src/app/(pages)/shapeless-method/page.tsx
+++ src/app/(pages)/shapeless-method/page.tsx
@@ -54,44 +54,9 @@ const Methods: MethodsType[] = [
 ];
 
 const ShapelessMethod = () => {
-  // Estado para controlar el índice activo
-  const [activeIndex, setActiveIndex] = useState(0);
-  const router = useRouter(); // Inicializar el enrutador
 
-  useEffect(() => {
-    const savedIndex = sessionStorage.getItem("methodIndex");
-    setActiveIndex(savedIndex ? parseInt(savedIndex, 10) : 0);
-  }, []);
 
-  useEffect(() => {
-    const handleKeyDown = (event: KeyboardEvent) => {
-      if (event.key === "ArrowRight") {
-        if (activeIndex === Methods.length) {
-          sessionStorage.setItem("methodIndex", activeIndex.toString());
-          router.push("/key-industries-expertise"); // Navega a la siguiente página
-        } else {
-          const newIndex = activeIndex + 1;
-          setActiveIndex(newIndex);
-          sessionStorage.setItem("methodIndex", newIndex.toString());
-        }
-      } else if (event.key === "ArrowLeft") {
-        if (activeIndex === 0) {
-          sessionStorage.setItem("methodIndex", activeIndex.toString());
-          router.push("/the-principles"); // Navega a la página anterior
-        } else {
-          const newIndex = activeIndex - 1;
-          setActiveIndex(newIndex);
-          sessionStorage.setItem("methodIndex", newIndex.toString());
-        }
-      }
-    };
 
-    window.addEventListener("keydown", handleKeyDown);
-
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-    };
-  }, [activeIndex, router]);
 
   return (
     <div className="flex flex-col h-screen items-center justify-center">
@@ -100,20 +65,15 @@ const ShapelessMethod = () => {
         prevPage="/the-principles"
         nextPage="/key-industries-expertise"
         visible={true}
-        disableKeyboardNavigation={true}
       />
 
       <main className="flex flex-1 flex-col items-center justify-center w-full h-full p-4">
-        {activeIndex === 0 && <MainContent methods={Methods} />}
-        {activeIndex === 1 && Methods[0].page}
-        {activeIndex === 2 && Methods[1].page}
-        {activeIndex === 3 && Methods[2].page}
+      <MainContent methods={Methods} />
+       
       </main>
 
       <Footer
-        title={
-          activeIndex === 0 ? "Find your own path and make it unique." : ""
-        }
+        title={ "Find your own path and make it unique."        }
         width="80vw"
         size="4xl"
       />
